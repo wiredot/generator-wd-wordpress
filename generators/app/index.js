62b2752b973de2238a7449f77567a53299092688
Piotr Soluch <psoluch@wiredot.com>
2017-11-24 15:55:47 +0100

    rearranged repository

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,181 +2,185 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var rename = require("gulp-rename");
 
 function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(find, 'g'), replace);
+	return str.replace(new RegExp(find, 'g'), replace);
 }
 
 function makeSalt() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-;:<>,./?|";
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-;:<>,./?|";
 
-    for ( var i=0; i < 64; i++ ) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
+	for ( var i=0; i < 64; i++ ) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
 
-    return text;
+	return text;
 }
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+	prompting: function () {
+		var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the awesome ' + chalk.red('WireDot WordPress') + ' Generator!'
-    ));
+		// Have Yeoman greet the user.
+		this.log(yosay(
+			'Welcome to the awesome ' + chalk.red('WireDot WordPress') + ' Generator!'
+		));
 
-    var prompts = [{
-      name: 'themeName',
-      message: 'Theme name',
-      default : replaceAll(this.appname, ' ', '.')
-    },
-    {
-      name: 'themeDir',
-      message: 'Theme directory',
-      default : replaceAll(this.appname, ' ', '.')
-    },
-    {
-      name: 'themeDescription',
-      message: 'Theme description',
-      default : 'Theme for ' + replaceAll(this.appname, ' ', '.')
-    },
-    {
-      name: 'themeUrl',
-      message: 'Theme Url',
-      default : replaceAll(this.appname, ' ', '.')
-    },
-    {
-      name: 'dbDatabase',
-      message: 'Database name',
-      default : replaceAll(this.appname, ' ', '_')
-    },
-    {
-      name: 'dbUsername',
-      message: 'Database Username',
-      default : 'admin'
-    },
-    {
-      name: 'dbPassword',
-      message: 'Database Password',
-      default : 'pass'
-    },
-    {
-      name: 'dbHost',
-      message: 'Database Host',
-      default : 'localhost'
-    },
-    {
-      name: 'dbPrefix',
-      message: 'Database Prefix',
-      default : 'wp' + ( Math.floor( Math.random() * 999 ) + 1 ) + '_'
-    },
-    {
-      type: 'confirm',
-      name: 'gitInit',
-      message: 'Init Git?',
-      default : true
-    }
-    ];
+		var prompts = [{
+			name: 'themeName',
+			message: 'Theme name',
+			default : replaceAll(this.appname, ' ', '.')
+		},
+		{
+			name: 'themeDir',
+			message: 'Theme directory',
+			default : replaceAll(this.appname, ' ', '.')
+		},
+		{
+			name: 'themeDescription',
+			message: 'Theme description',
+			default : 'Theme for ' + replaceAll(this.appname, ' ', '.')
+		},
+		{
+			name: 'themeUrl',
+			message: 'Theme Url',
+			default : replaceAll(this.appname, ' ', '.')
+		},
+		{
+			name: 'dbDatabase',
+			message: 'Database name',
+			default : replaceAll(this.appname, ' ', '_')
+		},
+		{
+			name: 'dbUsername',
+			message: 'Database Username',
+			default : 'admin'
+		},
+		{
+			name: 'dbPassword',
+			message: 'Database Password',
+			default : 'pass'
+		},
+		{
+			name: 'dbHost',
+			message: 'Database Host',
+			default : 'localhost'
+		},
+		{
+			name: 'dbPrefix',
+			message: 'Database Prefix',
+			default : 'wp' + ( Math.floor( Math.random() * 999 ) + 1 ) + '_'
+		},
+		{
+			type: 'confirm',
+			name: 'gitInit',
+			message: 'Init Git?',
+			default : true
+		}];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+		this.prompt(prompts, function (props) {
+			this.props = props;
 
-      done();
-    }.bind(this));
-  },
+			done();
+		}.bind(this));
+	},
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('skeleton/**/*'),
-      this.destinationPath('')
-    );
+	writing: function () {
+		var THAT = this;
+		this.registerTransformStream(
+			rename(function(path) {
+				path.basename = path.basename.replace(/(copernicus-blank)/g, THAT.props.themeDir);
+				path.dirname = path.dirname.replace(/(copernicus-blank)/g, THAT.props.themeDir);
+			})
+		);
 
-    this.fs.copy(
-      this.templatePath('theme/**/*'),
-      this.destinationPath('public/content/themes/' + this.props.themeDir)
-    );
+		this.fs.copy(
+			this.templatePath('copernicus-blank/**/*'),
+			this.destinationPath(''), {
+				themeDir: this.props.themeDir
+			}
+		);
 
-    this.fs.copyTpl(
-      this.templatePath('skeleton/_gitignore'),
-      this.destinationPath('.gitignore'),
-      {
-        themeDir: this.props.themeDir
-      }
-    );
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/_gitignore'),
+			this.destinationPath('.gitignore'),
+			{
+				themeDir: this.props.themeDir
+			}
+		);
 
-    this.fs.delete('_gitignore');
+		this.fs.delete('_gitignore');
 
-    this.fs.copyTpl(
-      this.templatePath('skeleton/bower.json'),
-      this.destinationPath('bower.json'),
-      {
-        themeName: this.props.themeName
-      }
-    );
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/bower.json'),
+			this.destinationPath('bower.json'),
+			{
+				themeName: this.props.themeName
+			}
+		);
 
-    this.fs.copyTpl(
-      this.templatePath('skeleton/wp-config.php'),
-      this.destinationPath('wp-config.php'),
-      {
-        authKey: makeSalt(),
-        secureAuthKey: makeSalt(),
-        loggedInKey: makeSalt(),
-        nonceKey: makeSalt(),
-        authSalt: makeSalt(),
-        secureAuthSalt: makeSalt(),
-        loggedInSalt: makeSalt(),
-        nonceSalt: makeSalt()
-      }
-    );
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/wp-config.php'),
+			this.destinationPath('wp-config.php'),
+			{
+				authKey: makeSalt(),
+				secureAuthKey: makeSalt(),
+				loggedInKey: makeSalt(),
+				nonceKey: makeSalt(),
+				authSalt: makeSalt(),
+				secureAuthSalt: makeSalt(),
+				loggedInSalt: makeSalt(),
+				nonceSalt: makeSalt()
+			}
+		);
 
-     this.fs.copyTpl(
-      this.templatePath('skeleton/.bowerrc'),
-      this.destinationPath('.bowerrc'),
-      {
-        themeDir: this.props.themeDir
-      }
-    );
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/.bowerrc'),
+			this.destinationPath('.bowerrc'),
+			{
+				themeDir: this.props.themeDir
+			}
+		);
 
-    this.fs.copyTpl(
-      this.templatePath('skeleton/.env'),
-      this.destinationPath('.env'),
-      {
-          dbDatabase: this.props.dbDatabase,
-          dbUsername: this.props.dbUsername,
-          dbPassword: this.props.dbPassword,
-          dbHost: this.props.dbHost,
-          dbPrefix: this.props.dbPrefix
-      }
-    );
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/.env'),
+			this.destinationPath('.env'),
+			{
+				dbDatabase: this.props.dbDatabase,
+				dbUsername: this.props.dbUsername,
+				dbPassword: this.props.dbPassword,
+				dbHost: this.props.dbHost,
+				dbPrefix: this.props.dbPrefix
+			}
+		);
 
-    this.fs.copyTpl(
-      this.templatePath('theme/style.css'),
-      this.destinationPath('public/content/themes/' + this.props.themeDir + '/style.css'),
-      {
-        themeName: this.props.themeName,
-        themeDescription: this.props.themeDescription,
-        themeUrl: this.props.themeUrl
-      }
-    );
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/public/content/themes/copernicus-blank/style.css'),
+			this.destinationPath('public/content/themes/' + this.props.themeDir + '/style.css'),
+			{
+				themeName: this.props.themeName,
+				themeDescription: this.props.themeDescription,
+				themeUrl: this.props.themeUrl
+			}
+		);
 
-    this.fs.copyTpl(
-      this.templatePath('theme/src/scss/style.scss'),
-      this.destinationPath('public/content/themes/' + this.props.themeDir + '/src/scss/style.scss'),
-      {
-        themeName: this.props.themeName
-      }
-    );
-  },
+		this.fs.copyTpl(
+			this.templatePath('copernicus-blank/public/content/themes/copernicus-blank//src/scss/style.scss'),
+			this.destinationPath('public/content/themes/' + this.props.themeDir + '/src/scss/style.scss'),
+			{
+				themeName: this.props.themeName
+			}
+		);
+	},
 
-  install: function () {
-    this.spawnCommand('composer', ['install']);
-    this.spawnCommand('bower', ['update']);
-    if (this.props.gitInit) {
-      this.spawnCommand('git', ['init']);
-    }
-    this.bowerInstall();
-  }
+	install: function () {
+		this.spawnCommand('composer', ['install']);
+		this.spawnCommand('bower', ['update']);
+		if (this.props.gitInit) {
+			this.spawnCommand('git', ['init']);
+		}
+		this.bowerInstall();
+	}
 });
